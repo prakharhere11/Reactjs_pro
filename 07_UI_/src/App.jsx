77ff9d1505e2acb5c65bd6bc3826bc3d4a6ebadb
prakharhere11 <prakharagrahari71@gmail.com>
2026-07-06 17:18:44 +0530

    completed

--- 07_UI_/src/App.jsx
+++ 07_UI_/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import Section1 from './components/Section1/section1'
-import Section2 from './components/Section2/Section2'
 const App = () => {
   return (
     <div>
       <Section1 />
-      <Section2 />
+      
     </div>
   )
 }
--- 07_UI_/src/components/Section2/Section2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Section2 = () => {
-  return (
-    <div className='h-screen w-full bg-blue-900'>SECTION 2</div>
-  )
-}
-
-export default Section2
